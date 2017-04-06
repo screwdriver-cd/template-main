dc7827b0de1b0a8b28a16544d7f1026e3729b72c
Jerry Zhang <thejerryzhang@gmail.com>
2017-04-06 16:18:11 -0700

    start validation stuff

--- index.js
+++ index.js
@@ -38,6 +38,48 @@ function publishTemplate() {
         .catch(err => Promise.reject(err.message));
 }
 
+/**
+ * Validates the template yaml by posting to the SDAPI /validator/template endpoint
+ * @method validateTemplate
+ * @return {Request}         Request object from post call to SDAPI
+ */
+function validateTemplate() {
+    let i;
+    let errorMessage;
+
+    const path = process.env.SD_TEMPLATE_PATH || './sd-template.yaml';
+    const json = JSON.stringify(Yaml.safeLoad(fs.readFileSync(path, 'utf8')));
+    const params = {
+        body: {
+            yaml: json
+        },
+        json: true,
+        auth: {
+            bearer: process.env.SD_TOKEN
+        },
+        method: 'POST',
+        url: 'https://api.screwdriver.cd/v4/validator/template'
+    };
+
+    return request(params, (err, response, body) => {
+        if (err) {
+            throw new Error(`Error sending validate request: ${err}`);
+        } else if (body.errors.length > 0) {
+            errorMessage = 'Template is not valid for the following reasons:';
+            for (i = 0; i < body.errors.length; i += 1) {
+                /* eslint-disable prefer-template */
+                errorMessage += `\n ${JSON.stringify(body.errors[i], null, 3)}`;
+                /* eslint-enable prefer-template */
+            }
+
+            throw new Error(errorMessage);
+        } else {
+            console.log('Template is valid.');
+        }
+    }).headers;
+}
+
 module.exports = {
-    publishTemplate
+    publishTemplate,
+    validateTemplate
 };
