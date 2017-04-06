79ae032744cd6fb6779e406d2ab6f8d540040bd5
Jerry Zhang <thejerryzhang@gmail.com>
2017-04-05 17:35:44 -0700

    feat: script for validating and publishing templates

---
+++ index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const request = require('request-promise-native');
+const fs = require('fs');
+const URL = require('url');
+const Yaml = require('js-yaml');
+
+/**
+ * Loads the yaml configuration from a file
+ * @method loadYaml
+ * @param  {String}     path        File path
+ * @return {Promise}    Promise that resolves to the template as a config object
+ */
+function loadYaml(path) {
+    return new Promise(resolve =>
+        resolve(Yaml.safeLoad(fs.readFileSync(path, 'utf8'))));
+}
+
+/**
+ * Validates the template yaml
+ * @method validateTemplate
+ * @param   {Object}        config          Template config
+ * @return {Promise}         Resolves when template is valid/rejects when error or invalid
+ */
+function validateTemplate(config) {
+    const hostname = process.env.SD_API_URL || 'https://api.screwdriver.cd/v4/';
+    const url = URL.resolve(hostname, 'validator/template');
+
+    return request({
+        method: 'POST',
+        url,
+        auth: {
+            bearer: process.env.SD_TOKEN
+        },
+        json: true,
+        body: {
+            yaml: JSON.stringify(config)
+        }
+    }).then((response) => {
+        if (response.errors.length > 0) {
+            let errorMessage = 'Template is not valid for the following reasons:';
+
+            response.errors.forEach((err) => {
+                /* eslint-disable prefer-template */
+                errorMessage += `\n${JSON.stringify(err, null, 4)},`;
+                /* eslint-enable prefer-template */
+            });
+
+            throw new Error(errorMessage);
+        }
+
+        return 'Template is valid';
+    });
+}
+
+/**
+ * Publishes the template yaml by posting to the SDAPI /templates endpoint
+ * @method publishTemplate
+ * @param   {Object}        config          Template config
+ * @return {Promise}        Resolves if publish successfully
+ */
+function publishTemplate(config) {
+    const hostname = process.env.SD_API_URL || 'https://api.screwdriver.cd/v4/';
+    const url = URL.resolve(hostname, 'templates');
+
+    return request({
+        method: 'POST',
+        url,
+        auth: {
+            bearer: process.env.SD_TOKEN
+        },
+        json: true,
+        body: {
+            yaml: JSON.stringify(config)
+        },
+        resolveWithFullResponse: true,
+        simple: false
+    }).then((response) => {
+        const body = response.body;
+
+        if (response.statusCode !== 201) {
+            throw new Error('Error publishing template. ' +
+            `${body.statusCode} (${body.error}): ${body.message}`);
+        }
+
+        return 'Template was successfully published';
+    });
+}
+
+module.exports = {
+    loadYaml,
+    validateTemplate,
+    publishTemplate
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,158 @@
 'use strict';
 
 const assert = require('chai').assert;
+const mockery = require('mockery');
+const sinon = require('sinon');
 
-describe('index test', () => {
-    it('fails', () => {
-        assert.isTrue(false);
+describe('index', () => {
+    let requestMock;
+    let YamlMock;
+    let fsMock;
+    let index;
+    let templateConfig;
+
+    before(() => {
+        mockery.enable({
+            warnOnUnregistered: false,
+            useCleanCache: true
+        });
+    });
+
+    beforeEach(() => {
+        requestMock = sinon.stub();
+        YamlMock = {
+            safeLoad: sinon.stub()
+        };
+        fsMock = {
+            readFileSync: sinon.stub()
+        };
+        templateConfig = {
+            name: 'template/test',
+            version: '1.0.0',
+            description: 'test description',
+            maintainer: 'foo@bar.com',
+            config: {
+                image: 'node:6',
+                steps: [
+                    { publish: 'node ./test.js' }
+                ]
+            }
+        };
+
+        YamlMock.safeLoad.returns(templateConfig);
+
+        mockery.registerMock('fs', fsMock);
+        mockery.registerMock('js-yaml', YamlMock);
+        mockery.registerMock('request-promise-native', requestMock);
+
+        // eslint-disable-next-line global-require
+        index = require('../index');
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+        index = null;
+    });
+
+    after(() => {
+        mockery.disable();
+    });
+
+    describe('Load Yaml', () => {
+        it('resolves if loads successfully', () => {
+            YamlMock.safeLoad.resolves('yamlcontent');
+
+            return index.loadYaml(config => assert.equal(config, 'yamlcontent'));
+        });
+    });
+
+    describe('Template Validate', () => {
+        it('throws error when request yields an error', () => {
+            requestMock.rejects(new Error('error'));
+
+            return index.validateTemplate(templateConfig)
+                .then(() => assert.fail('should not get here'))
+                .catch(err => assert.equal(err.message, 'error'));
+        });
+
+        it('throws error if response template is invalid', () => {
+            const responseFake = {
+                errors: [{
+                    message: '"steps" is required',
+                    path: 'config.steps',
+                    type: 'any.required',
+                    context: {
+                        key: 'steps'
+                    }
+                }],
+                template: {}
+            };
+
+            requestMock.resolves(responseFake);
+
+            return index.validateTemplate(templateConfig)
+                .then(() => assert.fail('should not get here'))
+                .catch(err => assert.equal(
+                    err.message,
+                    'Template is not valid for the following reasons:\n' +
+                    '{\n    "message": "\\"steps\\" is required",\n    "path": "config.steps",' +
+                    '\n    "type": "any.required",' +
+                    '\n    "context": {\n        "key": "steps"\n    }\n},')
+                );
+        });
+
+        it('resolves if template is valid', () => {
+            const responseFake = {
+                errors: [],
+                template: templateConfig
+            };
+
+            requestMock.resolves(responseFake);
+
+            return index.validateTemplate(templateConfig)
+                .then(msg => assert.equal(msg, 'Template is valid'));
+        });
+    });
+
+    describe('Template Publish', () => {
+        it('throws error when request yields an error', () => {
+            requestMock.rejects(new Error('error'));
+
+            return index.publishTemplate(templateConfig)
+                .then(() => assert.fail('should not get here'))
+                .catch(err => assert.equal(err.message, 'error'));
+        });
+
+        it('throws error for the corresponding request error status code if not 201', () => {
+            const responseFake = {
+                statusCode: 403,
+                body: {
+                    statusCode: 403,
+                    error: 'Forbidden',
+                    message: 'Fake forbidden message'
+                }
+            };
+
+            requestMock.resolves(responseFake);
+
+            return index.publishTemplate(templateConfig)
+                .then(() => assert.fail('should not get here'))
+                .catch(err => assert.equal(err.message,
+                    'Error publishing template. 403 (Forbidden): Fake forbidden message')
+                );
+        });
+
+        it('succeeds and does not throw an error if request status code is 201', () => {
+            const responseFake = {
+                statusCode: 201,
+                body: templateConfig
+            };
+
+            requestMock.resolves(responseFake);
+
+            return index.publishTemplate(templateConfig)
+                .then(msg => assert.equal(msg, 'Template was successfully published'));
+        });
     });
 });
